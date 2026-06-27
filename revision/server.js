2046941340cf45940a885867baeedcc9002526ac
ayan khan <ayankhanayan992@gmail.com>
2026-06-27 19:59:09 +0530

    rev of backend

---
+++ revision/server.js
@@ -0,0 +1,6 @@
+// server ko start karna 
+const app =require('./src/app')
+
+app.listen(3001,()=>{
+    console.log("server is running on port 3001")
+})
---
+++ revision/src/app.js
@@ -0,0 +1,74 @@
+const express=require('express');
+
+const app =express();  //server instance create kar rha hai
+
+/*
+notes=[
+{
+tittle:"server.js",
+description:"server ko start karne ke liye app.listen() method ka use kiya jata hai"
+}
+]
+*/ 
+
+app.use(express.json());
+const notes =[]
+//post methood ka use karke data ko server me bheja jata hai
+//tittle and discription ko post karne ke liye api banai hai
+app.post('/notes',(req,res)=>{
+    console.log(req.body);
+    notes.push(req.body);
+
+    res.status(201).json({
+        message:"note is added successfully",
+        
+    })
+})
+
+// GET method ka use karke data ko server se get kiya jata hai
+//GET methood
+
+app.get('/notes',(req,res)=>{
+    res.status(200).json({
+   message:"notes are fetched successfully",
+
+    notes:notes
+    })
+})
+
+
+// delete method ka use karke data ko server se delete kiya jata hai
+
+app.delete('/notes/:index',(req,res)=>{
+    const index=req.params.index;
+
+    delete notes[ index]
+
+    res.status(200).json({
+        message:"note is deleted successfully"
+    })
+})
+
+//patch method ka use karke data ko server me update kiya jata hai
+
+app.patch("/notes/:index",(req,res)=>{
+    const index=req.params.index
+    const description=req.body.description
+
+    notes[index].description=description
+
+    res.status(200).json({
+        message:"note is updated successfully"
+    })
+})
+
+
+
+
+
+
+
+
+
+
+module.exports=app;  //app ko export kar rha hai
